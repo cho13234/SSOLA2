2a6c744e07b0df1938618874ea919f14100d40ef
dkssm1206 <dkssm1206@naver.com>
2017-02-17 13:49:02 +0900

    .

--- SSOLA2/src/main/java/com/ssola2/controller/MypageController.java
+++ SSOLA2/src/main/java/com/ssola2/controller/MypageController.java
@@ -36,15 +36,15 @@ public class MypageController {
 	@Autowired
 	@Qualifier("memberDao")
 	private MemberDao dao;
-	
+
 	@Autowired
 	@Qualifier("memberService")
 	private MemberService memberService;
-	
+
 	@Autowired
 	@Qualifier("scrapService")
 	private ScrapService scrapService;
-	
+
 
 	//my profile
 	@RequestMapping(value = "mypage_main.action", method = RequestMethod.GET)
@@ -52,44 +52,44 @@ public class MypageController {
 
 		Member member = (Member)session.getAttribute("loginuser");
 		Profile p_list = memberService.selectProfile(member.getId());
-				
+
 		model.addAttribute("p_list", p_list);
-		
-		
-		
+
+
+
 		return "mypage/mypage_main";
 	}
-	
+
 	//회원정보 보기
 	@RequestMapping(value = "loginedit.action", method = RequestMethod.GET)
 	public String mypage_loginedit(HttpSession session, Model model , Customer customer, Member member)	
 	{		
 		customer = (Customer) session.getAttribute("loginuser");
-		
-		
+
+
 		return "mypage/loginedit";
 	}
-	
-	
+
+
 	//회원정보 수정<<보류 : 수정한 내용 적용 ㄴㄴ
 	@RequestMapping(value ="update_loginedit.action", method = RequestMethod.POST)
 	public String updateMember(@ModelAttribute Customer customer, HttpSession session)
 	{		
-			
-			String passwd = customer.getPassword();
-			
-			if (passwd == "") {
-				memberService.updatePasswdNTx(customer);
-			} else {
-				customer.setPassword(Util.getHashedString(customer.getPassword(), "SHA-256"));
-				memberService.updateMemberTx(customer);
-			}
-						
-			
-			return "redirect:/mypage/mypage_main.action";
+
+		String passwd = customer.getPassword();
+
+		if (passwd == "") {
+			memberService.updatePasswdNTx(customer);
+		} else {
+			customer.setPassword(Util.getHashedString(customer.getPassword(), "SHA-256"));
+			memberService.updateMemberTx(customer);
+		}
+
+
+		return "redirect:/mypage/mypage_main.action";
 
 	}
-	
+
 	@RequestMapping(value = "logindelete.action", method = RequestMethod.GET)
 	public String deleteMember(HttpSession session, Model model, Customer customer, Member member)	
 	{		
@@ -99,75 +99,23 @@ public class MypageController {
 		member.setId(id);
 		member.setDeleted(deleted);
 		memberService.deleteMember(member);
-		
-		
+
+
 		return "account/login";
 	}
-	
-	/////////////////////////scrap
-	@RequestMapping(value = "scrapform.action", method = RequestMethod.GET)
-	public ModelAndView mypage_scrapform(HttpSession session, ModelAndView mav)	
-	{
-		
-		Member member = (Member)session.getAttribute("loginuser");
-		
-		List<Scrap> list = scrapService.getListById(member.getId());
 
-		mav.setViewName("mypage/scrapform");
-		mav.addObject("list", list);
-		
-		return mav;
-	}
-	
-	@RequestMapping(value = "deleteScrap.action", method = RequestMethod.GET)
-	public String deleteScrap(@RequestParam int scrapNo)
-	{
-		
-		
-		scrapService.deleteScrap(scrapNo);
-		return "redirect:/mypage/scrapform.action";
-	}
-	
-	@RequestMapping(value = "insertScrap.action", method = RequestMethod.POST)
-	public String insertScrap()
-	{
-		
-		return "main/main";
-	}
-	
-	@RequestMapping(value = "scrapInfo.action", method = RequestMethod.GET)
-	public String scrapInfo(HttpSession session, @ModelAttribute Scrap scrap, int articleNo)	
-	{
-		
-		Member member = (Member)session.getAttribute("loginuser");
-		 scrap = scrapService.selectScrapInfo(articleNo);
-		 scrap.setId(member.getId());
-	
-		 scrapService.InsertScrapTx(scrap);
-
-		return "redirect:/main/main.action";
-	}
-	
-	///////////////////////email
-	@RequestMapping(value = "q_mail.action", method = RequestMethod.GET)
-	public String q_mail()
-	{
-		return "mypage/q_mail";
-	}
-	
-	
 	///////////////////////친구 프로필
 	@RequestMapping(value = "mypage_friendmain.action", method = RequestMethod.GET)
-//	@ResponseBody //String 값을 내보낼 때
+	//@ResponseBody //String 값을 내보낼 때
 	public String mypage_friendmain(Model model, String id, HttpSession session) {
-		
+
 		Member member = (Member)session.getAttribute("loginuser");
 		Profile p_list = memberService.selectProfile(id);
 		String sid = member.getId();	
 		System.out.println(sid);
 		List<Friend_list> mf_list = memberService.friendsStatus(sid);
 		model.addAttribute("p_list", p_list);
-		
+
 		for(Friend_list f_list : mf_list) {
 			// 넌 나의 친구 상태
 			if(f_list.getDestination_id().equals(id) && f_list.isDeleted() == true)  {
@@ -179,36 +127,87 @@ public class MypageController {
 				model.addAttribute("c", "c");
 			}
 		}
-		
+
 		return "mypage/mypage_main";
 	}
-	
+
 	////////////////친구 추가
 	@RequestMapping(value = "add_friend.action", method = RequestMethod.GET)
 	@ResponseBody
 	public String addFriend(HttpSession session, String id2 , String abc) {
-		
+
 		System.out.println(id2 +"확인중");
 		Member member = (Member)session.getAttribute("loginuser");
 		Friend_list f_list = new Friend_list();
 		f_list.setSource_id(member.getId());
 		f_list.setDestination_id(id2);
-		
+
 		List<Friend_list> mf_list = memberService.friendsStatus(member.getId());
-		
+
 		for(Friend_list f_list2 : mf_list) {
 			// 넌 나의 친구 상태
 			if(!f_list2.getDestination_id().equals(id2) && abc.equals("c"))  {
 				memberService.insertFriend(f_list);
 			}else if (f_list2.isDeleted() == true && abc.equals("a")){
-					memberService.updateFriend(f_list);
+				memberService.updateFriend(f_list);
 			} else if(f_list2.isDeleted() == false && abc.equals("b")) {
 				memberService.updateFriend1(f_list);
 			}
 		}
-		
-		
-		
 		return "a";		
 	}
+
+	/////////////////////////scrap
+	@RequestMapping(value = "scrapform.action", method = RequestMethod.GET)
+	public ModelAndView mypage_scrapform(HttpSession session, ModelAndView mav)	
+	{
+
+		Member member = (Member)session.getAttribute("loginuser");
+
+		List<Scrap> list = scrapService.getListById(member.getId());
+
+		mav.setViewName("mypage/scrapform");
+		mav.addObject("list", list);
+
+		return mav;
+	}
+
+	@RequestMapping(value = "deleteScrap.action", method = RequestMethod.GET)
+	public String deleteScrap(@RequestParam int scrapNo)
+	{
+
+
+		scrapService.deleteScrap(scrapNo);
+		return "redirect:/mypage/scrapform.action";
+	}
+
+	@RequestMapping(value = "insertScrap.action", method = RequestMethod.POST)
+	public String insertScrap()
+	{
+
+		return "main/main";
+	}
+
+	@RequestMapping(value = "scrapInfo.action", method = RequestMethod.GET)
+	public String scrapInfo(HttpSession session, @ModelAttribute Scrap scrap, int articleNo)	
+	{
+
+		Member member = (Member)session.getAttribute("loginuser");
+		scrap = scrapService.selectScrapInfo(articleNo);
+		scrap.setId(member.getId());
+
+		scrapService.InsertScrapTx(scrap);
+
+		return "redirect:/main/main.action";
+	}
+
+	///////////////////////email
+	@RequestMapping(value = "q_mail.action", method = RequestMethod.GET)
+	public String q_mail()
+	{
+		return "mypage/q_mail";
+	}
+
+
+
 }
